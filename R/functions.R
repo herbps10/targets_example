f4edbf030463d915764abc946d271b3d01fcdfcf
Herb Susmann <herbps10@gmail.com>
2021-04-08 13:57:21 -0400

    Added pipeline

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,17 @@
-analysis <- function(data) {
+analysis_data <- function() {
+  gapminder::gapminder
+}
 
+create_plot <- function(data) {
+  ggplot(data, aes(x = year, y = lifeExp, group = country)) +
+    geom_line() +
+    facet_wrap(~continent)
+}
+
+fit_model <- function(data) {
+  stan_lmer(lifeExp ~ year + scale(gdpPercap) + (1 | continent), data = data)
+}
+
+create_posterior_plot <- function(fit) {
+  bayesplot::mcmc_dens(fit, c("year", "scale(gdpPercap)"))
 }
--- _targets.R
+++ _targets.R
@@ -1,1 +1,30 @@
 library(targets)
+library(tarchetypes)
+
+source("R/functions.R")
+
+tar_option_set(packages = c("gapminder", "tidyverse", "rstanarm", "bayesplot"))
+
+list(
+  tar_target(
+    analysis_dataset,
+    analysis_data()
+  ),
+  
+  tar_target(
+    life_exp_plot,
+    create_plot(analysis_dataset)
+  ),
+  
+  tar_target(
+    fit,
+    fit_model(analysis_dataset)
+  ),
+  
+  tar_target(
+    posterior_plot,
+    create_posterior_plot(fit)
+  ),
+  
+  tar_render(report, "report.Rmd")
+)
